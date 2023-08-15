bc42d53415a2fe5cac197851362b34e1fd129f2a
Raphael C <polyster20@gmail.com>
2023-08-15 21:17:25 +0200

    Update index.js
    Add:
     - Router
     - CssBaseLine
     - AppBar
     - ThemeProvider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { theme } from './assets/theme';
 
+import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import { ResponsiveAppBar } from './components/AppBar/AppBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  { path: '/', element: 
+    <div> Accueil </div>
+  },
+  { path: '/login', element: <div> Login </div>},
+]);
+
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+
+      <ResponsiveAppBar />
+      
+      <CssBaseline />
+
+      <RouterProvider router={router} />
+
+    </ThemeProvider>
   </React.StrictMode>
 );
 
