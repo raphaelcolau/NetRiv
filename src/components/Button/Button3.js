3c1bb0e04f4ec192d2e91a545d0f9fa2cb891e91
Raphael C <polyster20@gmail.com>
2023-08-15 21:16:15 +0200

    Create Button3.js
    Creation of custom button to more closely resemble material design 3 buttons

---
+++ src/components/Button/Button3.js
@@ -0,0 +1,16 @@
+import { styled } from '@mui/material/styles';
+import { Button } from '@mui/material';
+
+const Button3 = styled(Button)(({ theme }) => ({
+    borderRadius: 500,
+    textTransform: 'unset',
+    fontWeight: 400,
+    lineHeight: 1.5,
+    padding: theme.spacing(1.2, 2),
+    boxShadow: 'none',
+    '&:hover': {
+        boxShadow: 'none',
+    },
+}));
+
+export default Button3;
