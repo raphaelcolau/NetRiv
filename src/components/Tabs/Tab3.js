0343f8e4d8927471cfc1cc943a7bcea059428235
Raphael C <polyster20@gmail.com>
2023-08-17 17:36:36 +0200

    Update Tab3.js

--- src/components/Tabs/Tab3.js
+++ src/components/Tabs/Tab3.js
@@ -7,6 +7,10 @@ const Tab3 = styled(Tab)(({ theme }) => ({
     [theme.breakpoints.down('tablet')]: {
         padding: theme.spacing(0, .5),
     },
+    [theme.breakpoints.down('mobile_l')]: {
+        padding: theme.spacing(0, 0),
+        minWidth: '70px',
+    },
 }));
 
 export default Tab3;
