263d1322a2ccdde3f652478e291187330bfe866d
Raphael C <polyster20@gmail.com>
2023-08-17 17:07:05 +0200

    Update Tab3.js

--- src/components/Tabs/Tab3.js
+++ src/components/Tabs/Tab3.js
@@ -4,6 +4,9 @@ import { Tab } from '@mui/material';
 const Tab3 = styled(Tab)(({ theme }) => ({
     textTransform: 'unset',
     padding: theme.spacing(0, 2.5),
+    [theme.breakpoints.down('tablet')]: {
+        padding: theme.spacing(0, .5),
+    },
 }));
 
 export default Tab3;
