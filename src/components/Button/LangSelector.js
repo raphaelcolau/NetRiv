b81781cf6297665648b698ca64b05671471b88c3
Raphael C <polyster20@gmail.com>
2023-08-17 16:32:19 +0200

    Create LangSelector.js
    Button to select your language

---
+++ src/components/Button/LangSelector.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Box, IconButton, Menu, MenuItem } from '@mui/material';
+import i18n from 'i18next';
+
+export default function SelectLanguageButton() {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => { setAnchorEl(event.currentTarget); };
+    const handleClose = lang => { 
+        setAnchorEl(null);
+        if (lang.length === 2 && typeof lang === "string") {i18n.changeLanguage(lang)};
+    };
+
+    const languages = (() => {
+        const l = [];
+        Object.keys(i18n.options.resources).forEach((key) => {
+            l.push(key)
+        })
+        return l;
+    })();
+
+    return (
+        <Box>
+            <IconButton 
+                variant="contained"
+                color="primary"
+                size="small"
+                aria-label="select language"
+                aria-controls={open ? 'language-menu' : undefined}
+                aria-haspopup="true"
+                onClick={handleClick}
+                sx={{
+                    fontSize: "1rem",
+                }}
+            >
+                {i18n.language.toUpperCase()}
+            </IconButton >
+
+            <Menu
+                id="language-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'language-button',
+                }}
+            >
+                {languages.map(language => (
+                    <MenuItem key={language} onClick={() => {handleClose(language)}}>{language.toUpperCase()}</MenuItem>
+                ))}
+            </Menu>
+
+        </Box>
+    );
+
+}
