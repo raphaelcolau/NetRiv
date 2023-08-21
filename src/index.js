8d31b1dcdd1eb862cd104771b59c17b7ba4fafab
Raphael C <polyster20@gmail.com>
2023-08-21 13:44:25 +0200

    i18n provider

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { theme } from './assets/theme';
 import { ResponsiveAppBar } from './components/AppBar/AppBar';
+import PageHome from './pages/home';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './assets/i18n.js';
 
-import './assets/i18n.js';
 import './index.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -15,7 +17,7 @@ import '@fontsource/roboto/700.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const router = createBrowserRouter([
-  { path: '/',      element: <div> Accueil </div> },
+  { path: '/',      element: <PageHome /> },
   { path: '/login', element: <div> Login </div>   },
 ]);
 
@@ -23,13 +25,13 @@ const router = createBrowserRouter([
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-
+      <I18nextProvider i18n={i18n}>
       <ResponsiveAppBar />
 
       <CssBaseline />
 
       <RouterProvider router={router} />
-
+      </I18nextProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
