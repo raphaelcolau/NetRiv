4be1efd9eeadda419e55538f6616c11da66ceb44
Raphael C <polyster20@gmail.com>
2023-08-21 13:44:51 +0200

    Create home.js

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import { Box, Grid, Typography, Paper } from '@mui/material';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { theme } from '../assets/theme';
+import Button3 from '../components/Button/Button3';
+
+
+export default function PageHome() {
+    return (
+        <Box>
+        </Box>
+    )
+}
