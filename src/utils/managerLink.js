21394be0c81cbc1172e141a749d4b112fd52611f
Raphael C <polyster20@gmail.com>
2023-10-02 12:51:00 +0200

    Update managerLink.js

--- src/utils/managerLink.js
+++ src/utils/managerLink.js
@@ -1,10 +1,6 @@
 export function managerLink(path, format) {
     const url = "https://manager.netriv.fr/";
 
-    if (!path.endsWith(".php")) {
-        path += ".php";
-    }
-
     if (format === "full") {
         return url + path;
     } else {
